dd86390764db23eba3f001efb7001c2386b77690
Upendra Kumar B R <Upendra.BR@mu-sigma.com>
2019-11-05 13:59:38 +0530

    Feature complete; CSS pending

--- src/app/multi-select/multi-select.component.ts
+++ src/app/multi-select/multi-select.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-multi-select',
@@ -8,24 +9,56 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MultiSelectComponent implements OnInit {
 
   @Input() data: string[];
+  filteredData: string[] = [];
   hideMenu: boolean = false
   optionsMap: object = {};
   constructor() { }
 
   ngOnInit() {
-    
-    this.data.map((datum) => {
+    this.filteredData = [...this.data];
+    this.filteredData.map((datum) => {
       this.optionsMap[datum] = false;
     })
-    console.log(this.optionsMap);
-    
+  }
+
+  updateSelectAll() {
+    let checkSelectAll = this.filteredData.map((datum) => {
+      return this.optionsMap[datum];
+    })
+    if(checkSelectAll.indexOf(false) !== -1){
+      $("#selectAllCb")[0].checked = false
+    } else {
+      $("#selectAllCb")[0].checked = true;
+    }
+  }
+
+  updateOptions(searchValue) {
+    this.filteredData = [...this.data];
+    this.filteredData = [...this.filteredData.filter((el) => {
+      return el.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1;
+    })];
+    this.updateSelectAll()
+  }
+
+  openDropdown() {
+    let searchValue = $('#searchField')[0].innerHTML
+    if (searchValue !== "") {
+      this.updateOptions(searchValue);
+    } else {
+      this.filteredData = [...this.data];
+      this.updateSelectAll()
+    }
   }
 
   selectAllToggle() {
-    console.log("Here");
-    this.data.map((datum) => {
-      this.optionsMap[datum] = !this.optionsMap[datum];;
+    this.filteredData.map((datum) => {
+      this.optionsMap[datum] = $("#selectAllCb")[0].checked;
     })
+  }
+
+  toggleEach(option) {
+    this.optionsMap[option] = !this.optionsMap[option];
+    this.updateSelectAll()
   }
 
 }
